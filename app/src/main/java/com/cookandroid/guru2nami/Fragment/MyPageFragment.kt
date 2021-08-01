f667fc8f0161f8334c82ef038e4e05d2671077c3
YejinS <ghwls0419@gmail.com>
2021-08-01 12:20:28 +0900

    데이터베이스 firebase 안드로이드 스튜디오랑 연동시킴

--- app/src/main/java/com/cookandroid/guru2nami/Fragment/MyPageFragment.kt
+++ app/src/main/java/com/cookandroid/guru2nami/Fragment/MyPageFragment.kt
@@ -5,34 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.LinearLayout
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 import com.cookandroid.guru2nami.R
-import kotlin.concurrent.fixedRateTimer
+import com.cookandroid.guru2nami.Adapters.MyPageListViewAdapter
 
 //마이페이지 화면
 class MyPageFragment : Fragment() {
 
-    lateinit var listView: ListView
-    val myPage_List = ArrayList<String>()  //마이페이지 항목들을 담을 String 리스트 생성
+    lateinit var myPageView: View
+    lateinit var listView : ListView
+
+    val myPageList = arrayListOf<String>(
+            "동네 설정하기",
+            "내 글 관리",
+            "고객 센터",
+            "환경 설정",
+            "내 글 관리"
+    )  //마이페이지 항목들을 담을 String 리스트 생성
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+        myPageView =  inflater.inflate(R.layout.fragment_my_page, container, false)
+        listView = myPageView.findViewById(R.id.myPageListView)
+        return myPageView
+    }
 
-        //View로 전환
-        val view:View = inflater.inflate(R.layout.fragment_my_page, container, false);
-
-        val listView:ListView = view.findViewById<ListView>(R.id.myPageListView)
-
-        myPage_List.add("동네 설정하기")
-        myPage_List.add("내 글 관리")
-        myPage_List.add("고객 센터")
-        myPage_List.add("환경 설정")
-        myPage_List.add("내 글 관리")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listView.apply {
+            var Adapter = MyPageListViewAdapter(context, myPageList)
+            listView.adapter = Adapter
+        }
+    }
+//        //View로 전환
+//        val view:View = inflater.inflate(R.layout.fragment_my_page, container, false);
+//
+//        val listView:ListView = view.findViewById<ListView>(R.id.myPageListView)
+//
+//        myPage_List.add("동네 설정하기")
+//        myPage_List.add("내 글 관리")
+//        myPage_List.add("고객 센터")
+//        myPage_List.add("환경 설정")
+//        myPage_List.add("내 글 관리")
 
 //        //어댑터 생성(data와 view를 연결해주는 관리자)
 //        val listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,myPage_List)
@@ -45,6 +60,5 @@ class MyPageFragment : Fragment() {
 //                    Toast.LENGTH_SHORT).show() //선택한 데이터 출력
 //        }
 
-    }
 
 }
