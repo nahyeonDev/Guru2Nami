19dd50d306b590749cb564f7622f7aafbda02f7a
silver0-stack <qmfpsp@naver.com>
2021-08-03 07:26:21 +0900

    파이어베이스 리얼타임 연동 완료  1. 이미지 4개 다중 선택 계속 안됨  2. 1번 완료 후 이제 렌더링하기

--- app/src/main/java/com/cookandroid/guru2nami/Content/PersonalWriteActivity.kt
+++ app/src/main/java/com/cookandroid/guru2nami/Content/PersonalWriteActivity.kt
@@ -1,13 +1,11 @@
 package com.cookandroid.guru2nami.Content
 
-import android.R.attr
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -20,19 +18,14 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import java.text.SimpleDateFormat
 import java.util.*
-import android.widget.Toast
-
-import android.content.ClipData
-
-import android.R.attr.data
-import java.lang.Exception
-
 
 //개인나눔 글쓰는 페이지
 class PersonalWriteActivity : AppCompatActivity() {
     //firebase
-    private var PICK_IMAGE_FROM_ALBUM = 100
-
+    private var PICK_IMAGE_FROM_ALBUM1 = 0 //앨범 픽 변수
+    private var PICK_IMAGE_FROM_ALBUM2 = 0 //앨범 픽 변수
+    private var PICK_IMAGE_FROM_ALBUM3 = 0 //앨범 픽 변수
+    private var PICK_IMAGE_FROM_ALBUM4 = 0 //앨범 픽 변수
     private var storage: FirebaseStorage? = null
     var photoUri: Uri? = null
     private lateinit var database: DatabaseReference
@@ -80,11 +73,9 @@ class PersonalWriteActivity : AppCompatActivity() {
 
         perImgPlus1.setOnClickListener {
             //앨범 열기
-            var photoPickerIntent = Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
-            photoPickerIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true); //사진을 여러장 선택할 수 있게 함
+            var photoPickerIntent = Intent(Intent.ACTION_PICK)
             photoPickerIntent.type = "image/*"
-            photoPickerIntent.action = Intent.ACTION_GET_CONTENT;
-            startActivityForResult(Intent.createChooser( photoPickerIntent, "Select Picture"), PICK_IMAGE_FROM_ALBUM)
+            startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM1)
         }
         perImgPlus2.setOnClickListener {
             //앨범 열기
@@ -132,68 +123,50 @@ class PersonalWriteActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-//        if (requestCode == PICK_IMAGE_FROM_ALBUM) {
-//            if (resultCode == Activity.RESULT_OK) {
-//                //선택된 이미지 path
-//                photoUri = data?.data
-//                perImgPlus1.setImageURI(photoUri)
-//            } else {
-//                Toast.makeText(this@PersonalWriteActivity, "사진 4장을 선택해주세요", Toast.LENGTH_SHORT)
-//                    .show()
-//                return@onActivityResult
-//            }
-//        }
-        try {
-            // When an Image is picked
-            if (requestCode ===  PICK_IMAGE_FROM_ALBUM && resultCode ===  Activity.RESULT_OK && null != attr.data) {
-                // Get the Image from data
-                val filePathColumn = arrayOf(MediaStore.Images.Media._ID)
-               val imagesEncodedList = ArrayList<String>()
-                if (attr.data.getData() != null) {
-                    val mImageUri: Uri = attr.data.getData()
-
-                    // Get the cursor
-                    val cursor: Cursor? = contentResolver.query(
-                        mImageUri,
-                        filePathColumn, null, null, null
-                    )
-                    // Move to first row
-                    cursor.moveToFirst()
-                    val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
-                    val imageEncoded = cursor.getString(columnIndex)
-                    cursor.close()
-                } else {
-                    if (attr.data.getClipData() != null) {
-                        val mClipData: ClipData = attr.data.getClipData()
-                        val mArrayUri = ArrayList<Uri>()
-                        for (i in 0 until mClipData.itemCount) {
-                            val item = mClipData.getItemAt(i)
-                            val uri = item.uri
-                            mArrayUri.add(uri)
-                            // Get the cursor
-                            val cursor: Cursor? =
-                                contentResolver.query(uri, filePathColumn, null, null, null)
-                            // Move to first row
-                            cursor.moveToFirst()
-                            val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
-                            imageEncoded = cursor.getString(columnIndex)
-                            imagesEncodedList.add(imageEncoded)
-                            cursor.close()
-                        }
-                        Log.v("LOG_TAG", "Selected Images" + mArrayUri.size())
-                    }
-                }
+        if (requestCode == PICK_IMAGE_FROM_ALBUM1) {
+            if (resultCode == Activity.RESULT_OK) {
+                //선택된 이미지 path
+                photoUri = data?.data
+                perImgPlus1.setImageURI(photoUri)
             } else {
-                Toast.makeText(
-                    this, "You haven't picked Image",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        } catch (e: Exception) {
-            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
-                .show()
-        }
+                Toast.makeText(this@PersonalWriteActivity, "사진 4장을 선택해주세요", Toast.LENGTH_SHORT)
+                    .show()
 
+            }
+        }
+       else if (requestCode == PICK_IMAGE_FROM_ALBUM2) {
+            if (resultCode == Activity.RESULT_OK) {
+                //선택된 이미지 path
+                photoUri = data?.data
+                perImgPlus2.setImageURI(photoUri)
+            } else {
+                Toast.makeText(this@PersonalWriteActivity, "사진 4장을 선택해주세요", Toast.LENGTH_SHORT)
+                    .show()
+
+            }
+        }
+      else  if (requestCode == PICK_IMAGE_FROM_ALBUM3) {
+            if (resultCode == Activity.RESULT_OK) {
+                //선택된 이미지 path
+                photoUri = data?.data
+                perImgPlus3.setImageURI(photoUri)
+            } else {
+                Toast.makeText(this@PersonalWriteActivity, "사진 4장을 선택해주세요", Toast.LENGTH_SHORT)
+                    .show()
+
+            }
+        }
+        else if (requestCode == PICK_IMAGE_FROM_ALBUM4) {
+            if (resultCode == Activity.RESULT_OK) {
+                //선택된 이미지 path
+                photoUri = data?.data
+                perImgPlus4.setImageURI(photoUri)
+            } else {
+                Toast.makeText(this@PersonalWriteActivity, "사진 4장을 선택해주세요", Toast.LENGTH_SHORT)
+                    .show()
+
+            }
+        }
     }
 
     @SuppressLint("SimpleDateFormat")
