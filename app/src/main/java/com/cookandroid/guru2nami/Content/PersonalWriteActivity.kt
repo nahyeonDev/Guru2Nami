a03c7bc17bb35549888711ac67e583a886110bfb
YejinS <ghwls0419@gmail.com>
2021-08-03 03:17:17 +0900

    게시글 쓰기 view 에 해당 글 쓰고 게시물 등록 버튼 누르면 파이어베이스에 입력 데이터들이 등록되게 해주었습니당

--- app/src/main/java/com/cookandroid/guru2nami/Content/PersonalWriteActivity.kt
+++ app/src/main/java/com/cookandroid/guru2nami/Content/PersonalWriteActivity.kt
@@ -7,22 +7,28 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
-import androidx.core.content.ContextCompat
+import com.cookandroid.guru2nami.PostingData
 import com.cookandroid.guru2nami.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.jar.Manifest
 
 //개인나눔 글쓰는 페이지
 class PersonalWriteActivity : AppCompatActivity() {
+    //firebase
     private var PICK_IMAGE_FROM_ALBUM=0 //앨범 픽 변수
     private var storage:FirebaseStorage?=null
     var photoUri: Uri?=null
+    private lateinit var database: DatabaseReference
+    //이미지 등록
     lateinit var perImgPlus:ImageView
+    //글쓰기 항목들
     lateinit var perTitle:EditText
-    lateinit var Product2:EditText
-    lateinit var sharingMethod:RadioGroup
+    lateinit var product2:EditText
+    lateinit var methodTrans:EditText
     lateinit var category2: EditText
     lateinit var hopeArea:EditText
     lateinit var howTrans:EditText
@@ -33,10 +39,14 @@ class PersonalWriteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.action_personal_write)
 
-        perTitle=findViewById(R.id.perTitle)
-        Product2=findViewById(R.id.Product2)
+        //firebase
+        database = Firebase.database.reference
+        //이미지 등록
         perImgPlus=findViewById(R.id.perImgPlus)
-        sharingMethod=findViewById(R.id.sharingMethod)
+        //글쓰기 항목들
+        perTitle=findViewById(R.id.perTitle)
+        product2=findViewById(R.id.product2)
+        methodTrans=findViewById(R.id.methodTrans)
         category2=findViewById(R.id.category2)
         hopeArea=findViewById(R.id.hopeArea)
         howTrans=findViewById(R.id.howTrans)
@@ -57,6 +67,18 @@ class PersonalWriteActivity : AppCompatActivity() {
             personalRegisterButton.setOnClickListener{
                 contentUpload()
             }
+        }
+
+        personalRegisterButton.setOnClickListener{
+            val perTitle = perTitle.text.toString()
+            val product2 = product2.text.toString()
+            val methodTrans = methodTrans.text.toString()
+            val category2 = category2.text.toString()
+            val hopeArea = hopeArea.text.toString()
+            val howTrans = howTrans.text.toString()
+            val content2 = content2.text.toString()
+
+            writeNewPost("1", perTitle, product2, methodTrans, category2, hopeArea, howTrans, content2)
         }
     }
 
@@ -84,6 +106,17 @@ class PersonalWriteActivity : AppCompatActivity() {
         //파일업로드
         storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
             Toast.makeText(this@PersonalWriteActivity,"upload_success",Toast.LENGTH_LONG).show()
+
         }
+    }
+
+
+    private fun writeNewPost(postID: String, perTitle: String,
+                             product2: String, methodTrans: String,
+                             category2: String, hopeArea: String,
+                             howTrans: String, content2: String
+    ) {
+        val newPost = PostingData(perTitle, product2, methodTrans, category2, hopeArea, howTrans, content2)
+        database.child("PostingData").child(postID).setValue(newPost)
     }
 }
