405d1504329616f3ce58aaa23180c30518a14d7b
YejinS <ghwls0419@gmail.com>
2021-08-04 14:34:57 +0900

    홈화면-공구이벤트에서도 세부페이지로 넘어가기 기능 추가 + 세부페이지 xml 정리/ 수정

--- app/src/main/java/com/cookandroid/guru2nami/Adapters/ListTogetherAdapter.kt
+++ app/src/main/java/com/cookandroid/guru2nami/Adapters/ListTogetherAdapter.kt
@@ -1,11 +1,14 @@
 package com.cookandroid.guru2nami.Adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.cookandroid.guru2nami.Content.DetailViewActivity
 import com.cookandroid.guru2nami.R
+import com.cookandroid.guru2nami.User.Personal
 import com.cookandroid.guru2nami.User.Together
 
 //공구화면 리스트뷰와 카드뷰 연결 어댑터
@@ -14,7 +17,16 @@ class ListTogetherAdapter (private val togList : ArrayList<Together>) : Recycler
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListTogetherAdapter.MyViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_tog_list,parent,false)
-        return MyViewHolder(itemView)
+        return MyViewHolder(itemView).apply{
+            itemView.setOnClickListener {
+                val curPos : Int = adapterPosition  //curPos는 현재 클릭하는 포지션
+                val personal: Together = togList.get(curPos)  //userList는 Personal 클래스 안에 있는 변수 항목들
+//                Toast.makeText(parent.context, "희망 지역 : ${personal.perTitle}", Toast.LENGTH_SHORT).show()
+
+                var intent = Intent(parent.context, DetailViewActivity::class.java)
+                parent.context.startActivity(intent)
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: ListTogetherAdapter.MyViewHolder, position: Int) {
