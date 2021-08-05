677f3f7b59efe0a974a501f6f9cda265b976be75
nahyu <nahyeon.dev@gmail.com>
2021-08-05 20:35:52 +0900

    세부 화면 연결하기 위해 공구용 세부화면 만듦

--- app/src/main/java/com/cookandroid/guru2nami/Adapters/ListTogetherAdapter.kt
+++ app/src/main/java/com/cookandroid/guru2nami/Adapters/ListTogetherAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.cookandroid.guru2nami.Content.DetailViewActivity
+import com.cookandroid.guru2nami.Content.DetailViewActivity2
 import com.cookandroid.guru2nami.R
 import com.cookandroid.guru2nami.User.Personal
 import com.cookandroid.guru2nami.User.Together
@@ -33,13 +34,16 @@ class ListTogetherAdapter (private val togList : ArrayList<Together>) : Recycler
                 val content = content3.text.toString()
                 val id = userName2.text.toString()
                 val cate = category.text.toString()
+                val imgD = imgDetail2.text.toString()
 
-                var intent = Intent(parent.context, DetailViewActivity::class.java)
+                var intent = Intent(parent.context, DetailViewActivity2::class.java)
                 intent.putExtra("Title", title) /*송신*/
                 intent.putExtra("hopeArea", area)
                 intent.putExtra("Content", content)
                 intent.putExtra("id", id)
                 intent.putExtra("category", cate)
+                intent.putExtra("img",imgD)
+
                 parent.context.startActivity(intent)
             }
         }
@@ -48,13 +52,14 @@ class ListTogetherAdapter (private val togList : ArrayList<Together>) : Recycler
     override fun onBindViewHolder(holder: ListTogetherAdapter.MyViewHolder, position: Int) {
         val currentitem = togList[position]
 
-        val imageName : String? = currentitem.image3 //저장된 이미지 이름 받아오기
-        val imgUrl : String = "https://firebasestorage.googleapis.com/" +
-                "v0/b/nami-market.appspot.com/o/images%2F"+ imageName +
+        val imageName2 : String? = currentitem.image2 //저장된 이미지 이름 받아오기
+        val imgUrl2 : String = "https://firebasestorage.googleapis.com/" +
+                "v0/b/nami-market.appspot.com/o/images%2F"+ imageName2 +
                 "?alt=media&token=8770eebd-9052-4fe7-9e1a-a70273921fbf" //이미지 url
 
-        Glide.with(holder.itemView).load(imgUrl).into(holder.image4) //이미지 배치할 곳에 url 로드
+        Glide.with(holder.itemView).load(imgUrl2).into(holder.image3) //이미지 배치할 곳에 url 로드
 
+        holder.imgDetail2.text = currentitem.image2
         holder.togTitle.text = currentitem.togTitle
         holder.hopeArea2.text = currentitem.hopeArea2
         holder.content3.text = currentitem.content3
@@ -68,7 +73,8 @@ class ListTogetherAdapter (private val togList : ArrayList<Together>) : Recycler
     }
 
     class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-        val image4 : ImageView = itemView.findViewById(R.id.image_tog)
+        val imgDetail2 : TextView = itemView.findViewById(R.id.imgDetail2)
+        val image3 : ImageView = itemView.findViewById(R.id.image_tog)
         val togTitle : TextView = itemView.findViewById(R.id.title_tog)
         val hopeArea2 : TextView = itemView.findViewById(R.id.area_tog)
         val content3 : TextView = itemView.findViewById(R.id.content_main2)
--- app/src/main/java/com/cookandroid/guru2nami/Content/DetailViewActivity2.kt
+++ app/src/main/java/com/cookandroid/guru2nami/Content/DetailViewActivity2.kt
@@ -6,7 +6,6 @@ import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
@@ -15,8 +14,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
-import com.bumptech.glide.Glide
 import com.cookandroid.guru2nami.*
 import com.cookandroid.guru2nami.Adapters.PhotoViewAdapter
 import com.google.firebase.auth.FirebaseAuth
@@ -26,9 +23,8 @@ import com.google.firebase.ktx.Firebase
 import java.net.URL
 import kotlin.concurrent.thread
 
-
 //상세페이지
-class DetailViewActivity : AppCompatActivity() {
+class DetailViewActivity2 : AppCompatActivity() {
 
     private val REQUEST_READ_EXTERNAL_STORAGE = 1000
     //lateinit var viewPager : ViewPager
@@ -63,39 +59,39 @@ class DetailViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detailview)
+        setContentView(R.layout.activity_detailview_tog)
 
-        backBtn = findViewById(R.id.backBtn)
-        goToChat = findViewById(R.id.goToChat)
-        okBtn = findViewById(R.id.okBtn)
-        likeBtn = findViewById(R.id.likeIcon)
+        backBtn = findViewById(R.id.backBtn2)
+        goToChat = findViewById(R.id.goToChat2)
+        okBtn = findViewById(R.id.okBtn2)
+        likeBtn = findViewById(R.id.likeIcon2)
 
-        hopeArea = findViewById(R.id.hopeArea)
-        title = findViewById(R.id.title)
-        content = findViewById(R.id.content)
-        userId = findViewById(R.id.user_id)
-        category = findViewById(R.id.category)
-        imageMain = findViewById(R.id.imgViewMain)
+        hopeArea = findViewById(R.id.hopeArea2)
+        title = findViewById(R.id.title2)
+        content = findViewById(R.id.content2)
+        userId = findViewById(R.id.user_id2)
+        category = findViewById(R.id.category2)
+        imageMain = findViewById(R.id.imgViewMain2)
 
         //권한이 부여되었는지 확인
         if(ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
 
         //권한이 허용되지 않음
             if(ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                            Manifest.permission.READ_EXTERNAL_STORAGE)) {
                 //이전에 거부한 적이 있으면 설명(경고)
                 var dlg = AlertDialog.Builder(this)
                 dlg.setTitle("권한이 필요한 이유")
                 dlg.setMessage("사진 정보를 얻기 위해서는 외부 저장소 권한이 필수로 필요합니다.")
-                dlg.setPositiveButton("확인"){ dialog, which -> ActivityCompat.requestPermissions(this@DetailViewActivity,
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)}
+                dlg.setPositiveButton("확인"){ dialog, which -> ActivityCompat.requestPermissions(this@DetailViewActivity2,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)}
                 dlg.setNegativeButton("취소", null)
                 dlg.show()
             }else {
                 //처음 권한 요청
-                ActivityCompat.requestPermissions(this@DetailViewActivity,
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+                ActivityCompat.requestPermissions(this@DetailViewActivity2,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
             } else {
             //권한이 이미 제대로허용됨
             // getAllPhotos()
@@ -124,7 +120,7 @@ class DetailViewActivity : AppCompatActivity() {
                 "v0/b/nami-market.appspot.com/o/images%2F"+ urlImg+
                 "?alt=media&token=8770eebd-9052-4fe7-9e1a-a70273921fbf"
 
-        setImage(imgUrl)
+        setImage2(imgUrl)
 
 
         database = Firebase.database.reference
@@ -133,7 +129,7 @@ class DetailViewActivity : AppCompatActivity() {
         goToChat.setOnClickListener {
             posting()
             val intent = Intent(this, MainActivity::class.java)
-            Toast.makeText(this@DetailViewActivity, "채팅에서 만나요 :)", Toast.LENGTH_LONG).show()
+            Toast.makeText(this@DetailViewActivity2, "채팅에서 만나요 :)", Toast.LENGTH_LONG).show()
             startActivity(intent)
         }
 
@@ -141,14 +137,14 @@ class DetailViewActivity : AppCompatActivity() {
         okBtn.setOnClickListener {
             postBuying()
             val intent = Intent(this, MainActivity::class.java)
-            Toast.makeText(this@DetailViewActivity, "주문 완료!", Toast.LENGTH_LONG).show()
+            Toast.makeText(this@DetailViewActivity2, "주문 완료!", Toast.LENGTH_LONG).show()
             startActivity(intent)
         }
 
         //찜 버튼
         likeBtn.setOnClickListener {
             postLiking()
-            Toast.makeText(this@DetailViewActivity, "찜\uD83D\uDC9C", Toast.LENGTH_LONG).show()
+            Toast.makeText(this@DetailViewActivity2, "찜\uD83D\uDC9C", Toast.LENGTH_LONG).show()
         }
 
         //뒤로가기 버튼
@@ -168,8 +164,8 @@ class DetailViewActivity : AppCompatActivity() {
         val userId = userId.text.toString()
 
         makeNewPost(//글 업로드
-            chatTitle,
-            userId
+                chatTitle,
+                userId
         )
     }
 
@@ -178,7 +174,7 @@ class DetailViewActivity : AppCompatActivity() {
         val buyTitle= title.text.toString()
 
         makeBuyPost(//글 업로드
-            buyTitle
+                buyTitle
         )
     }
 
@@ -187,17 +183,17 @@ class DetailViewActivity : AppCompatActivity() {
         val likeTitle= title.text.toString()
 
         makeLikePost(//글 업로드
-            likeTitle
+                likeTitle
         )
     }
 
     private fun getAllPhotos() {
         val cursor = contentResolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            null,  //가져올 항목 배열
-            null,  //조건
-            null,  //조건
-            MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC") //날짜순
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null,  //가져올 항목 배열
+                null,  //조건
+                null,  //조건
+                MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC") //날짜순
 
         val fragments = ArrayList<Fragment>()
 
@@ -217,8 +213,8 @@ class DetailViewActivity : AppCompatActivity() {
     }
 
     private fun makeNewPost(
-        chatTitle : String,
-        userId : String
+            chatTitle : String,
+            userId : String
     ) {
         val key = database.child("Chat").push().key
         if (key == null) {
@@ -226,8 +222,8 @@ class DetailViewActivity : AppCompatActivity() {
             return
         }
         val newPost = ChatPosting(
-            chatTitle,
-            userId
+                chatTitle,
+                userId
 
         )
         database.child("Chat").child(key).setValue(newPost).addOnSuccessListener{
@@ -236,7 +232,7 @@ class DetailViewActivity : AppCompatActivity() {
     }
 
     private fun makeBuyPost(
-        buyTitle : String
+            buyTitle : String
     ) {
         val key = database.child("Buy").push().key
         if (key == null) {
@@ -244,7 +240,7 @@ class DetailViewActivity : AppCompatActivity() {
             return
         }
         val newPost = BuyPosting(
-            buyTitle
+                buyTitle
         )
         database.child("Buy").child(key).setValue(newPost).addOnSuccessListener{
 
@@ -252,7 +248,7 @@ class DetailViewActivity : AppCompatActivity() {
     }
 
     private fun makeLikePost(
-        likeTitle : String
+            likeTitle : String
     ) {
         val key = database.child("Like").push().key
         if (key == null) {
@@ -260,14 +256,14 @@ class DetailViewActivity : AppCompatActivity() {
             return
         }
         val newPost = LikePosting(
-            likeTitle
+                likeTitle
         )
         database.child("Like").child(key).setValue(newPost).addOnSuccessListener{
 
         }
     }
 
-    private fun setImage(url:String) {
+    private fun setImage2(url:String) {
         // 네트워크로 이미지를 불러오기 위해 서브스레드 처리
         thread(start=true) {
             val urlConnection = URL(url)
