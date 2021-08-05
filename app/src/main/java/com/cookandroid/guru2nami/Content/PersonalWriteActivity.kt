face093388f7104e879c2e01a7958733ab2ed1c6
nahyu <nahyeon.dev@gmail.com>
2021-08-05 15:02:02 +0900

    홈 화면 디자인 변경, 오류 고침

--- app/src/main/java/com/cookandroid/guru2nami/Content/PersonalWriteActivity.kt
+++ app/src/main/java/com/cookandroid/guru2nami/Content/PersonalWriteActivity.kt
@@ -22,7 +22,7 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
-import kotlinx.android.synthetic.main.action_personal_write.*
+//import kotlinx.android.synthetic.main.action_personal_write.*
 import java.text.SimpleDateFormat
 import java.util.*
 
